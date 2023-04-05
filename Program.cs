1761e059be18ad7d2ec97a4796933c7a0880f2f8
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-04-05 15:13:23 +0300

    program.cs değiştirildi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       Console.Write("Dairenin yarı çapını girin: ");
+       int radius = int.Parse(Console.ReadLine());
+
+       DrawCircle(radius);
+
+       static void DrawCircle(int radius)
+       {
+        for (double x = -radius; x <= radius; x++)
+        {
+            for (double y = -radius; y <= radius; y++)
+            {
+                if (x * x + y * y >= radius * radius)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+       }
+    }
+}
